519d16a33dcd57d6a49e259437215c03c01b6fcf
sandhu2702 <sk0581822@gmail.com>
2025-08-25 12:17:20 +0530

    Binary representation of a number n and number of visible people in right direction' s code commited

---
+++ Stack/Problems/numOfVisiblePeople.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    int arr[]={3,1,2,7,4,6,2,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int nge[n];
+    stack<int> st;
+    nge[n-1]=-1;
+    st.push(arr[n-1]);
+    for(int i=n-2;i>=0;i--){
+        while(!st.empty() && st.top()<=arr[i]){
+            st.pop();
+        }
+        //mark the ans in nge array
+        if(st.empty()){
+            nge[i]=-1;
+        }else{
+            nge[i]=st.top();
+        }
+        //push the arr[i] in stack
+        st.push(arr[i]);
+    }
+    for(int i=0;i<n;i++){
+            cout<<nge[i]<<" ";
+    }
+    cout<<endl;
+    int count=0;
+     for(int i=0;i<n;i++){
+        if(nge[i]!=-1){
+            count++;
+        }
+     }
+     cout<<count<<endl;
+}
