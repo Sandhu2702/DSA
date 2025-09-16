66fa9e7c287f248ad3502d2dc4c710856a83f158
sandhu2702 <sk0581822@gmail.com>
2025-09-16 16:30:44 +0530

    kth smallest number in an array with help of priority queue

---
+++ Heaps/kthSmallestElement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+using namespace std;
+int main(){
+     vector<int> arr={10,20,-4,6,18,24,105,18};
+     int k;
+     cout<<"kth number:";
+     cin>>k;
+     priority_queue<int> pq;
+     for(int i=0;i<arr.size();i++){
+        pq.push(arr[i]);
+        if(pq.size()>k){
+            pq.pop();
+        }
+     }
+     cout<<pq.top();
+}
