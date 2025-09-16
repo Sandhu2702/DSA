49b9ab51437fa5a336d33f14742e47fc01b0bf95
sandhu2702 <sk0581822@gmail.com>
2025-09-16 15:37:58 +0530

    basic maxHEap with help of STL

---
+++ Heaps/basicMaxHeap.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    priority_queue<int> pq;
+    pq.push(10);
+    pq.push(2);
+    pq.push(-6);
+    pq.push(80);
+    pq.push(40);
+    pq.push(30);
+    pq.push(0);
+    cout<<pq.top();
+    pq.pop();
+    cout<<endl;
+    cout<<pq.top();
+}
+
