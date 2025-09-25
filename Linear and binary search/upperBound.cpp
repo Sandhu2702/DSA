30b6ad435716cfc703e5caff176682beb5df35b3
sandhu2702 <sk0581822@gmail.com>
2025-09-25 17:44:27 +0530

    binary search introduced in upperBound

--- Linear and binary search/upperBound.cpp
+++ Linear and binary search/upperBound.cpp
@@ -4,10 +4,24 @@ int main(){
     int arr[9]={1,2,4,5,9,15,18,21,24};
     int n=9;
     int x=20;
-    for(int i=0;i<n;i++){
-        if(arr[i]>x){
-            cout<<arr[i];
+    // for(int i=0;i<n;i++){
+    //     if(arr[i]>x){
+    //         cout<<arr[i];
+    //         break;
+    //     }
+    // }
+    int lo=0;
+    int hi=n-1;
+    bool flag=false;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(arr[mid]==x){
+            flag=true;
+            cout<<arr[mid+1];
             break;
         }
+        else if(arr[mid]>x) hi=mid-1;
+        else lo=mid+1;
     }
+    if(flag==false) cout<<arr[lo];
 } 
