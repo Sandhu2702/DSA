a35eca20f8b8f9ee927900095a29d6f53eff5448
sandhu2702 <sk0581822@gmail.com>
2025-09-28 18:32:07 +0530

    pushing zeroes to end by maintaining relative order of all elements

---
+++ Sorting/Bubble Sort/zeroesToEnd.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[]={5,0,1,2,0,0,4,0,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<n-1;i++){
+        //traverse
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]==0){//swap
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
