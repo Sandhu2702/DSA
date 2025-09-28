cff42e522d2e85a0f9499c5bc366a93e0a3b4349
sandhu2702 <sk0581822@gmail.com>
2025-09-28 17:53:02 +0530

    optimized bubble sort added

--- Sorting/Bubble Sort/bubbleSort.cpp
+++ Sorting/Bubble Sort/bubbleSort.cpp
@@ -2,21 +2,35 @@
 #include<vector>
 using namespace std;
 int main(){
-    int arr[]={5,4,6,3,2,1,0,-4};
+    int arr[]={5,1,2,3,4};
     int n=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-    for(int i=0;i<n-1;i++){//n-1 passes
+    // for(int i=0;i<n-1;i++){//n-1 passes
+    //     //traverse
+    //     for(int j=0;j<n-1-i;j++){
+    //         if(arr[j]>arr[j+1]){// swap
+    //             // int temp=arr[j];
+    //             // arr[j]=arr[j+1];
+    //             // arr[j+1]=temp;
+    //             swap(arr[j],arr[j+1]);
+    //         }
+    //     }
+    // }
+    // optimized bubble sort----------------------------->
+    for(int i=0;i<n-1;i++){
         //traverse
+        bool flag=true;
         for(int j=0;j<n-1-i;j++){
             if(arr[j]>arr[j+1]){// swap
-                // int temp=arr[j];
-                // arr[j]=arr[j+1];
-                // arr[j+1]=temp;
                 swap(arr[j],arr[j+1]);
+                flag=false;
             }
+        }
+        if(flag==true){//swap didn't happen
+            break;
         }
     }
     for(int i=0;i<n;i++){
