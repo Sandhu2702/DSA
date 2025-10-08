176ba1bb8b2d6a7930ec5dbb0747f05e1c72de1c
sandhu2702 <sk0581822@gmail.com>
2025-10-08 17:36:47 +0530

    quick sort

---
+++ Sorting/Quick Sort/quickSort.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int partition(int arr[], int si, int ei){
+    int pivotElement=arr[si];
+    int count=0;
+    for(int i=si+1;i<=ei;i++){
+        if(arr[i]<=pivotElement){
+            count++;
+        }
+    }
+    int pivotIdx=count+si;
+    swap(arr[si],arr[pivotIdx]);
+    int i=si;
+    int j=ei;
+    while(i<pivotIdx && j>pivotIdx){
+        if(arr[i]<=arr[pivotIdx]) i++;
+        if(arr[j]>arr[pivotIdx]) j--;
+        else if(arr[i]>arr[pivotIdx] && arr[j]<=arr[pivotIdx]){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+    return pivotIdx;
+}
+void quickSort(int arr[], int si, int ei){//si-starting index and ei-ending index
+    if(si>=ei) return;
+    //5,1,8,2,7,6,3,4
+    int pi=partition(arr,si,ei);// pi-pivot index
+    //4,1,3,2,5,7,8,6
+    quickSort(arr,si,pi-1);
+    quickSort(arr,pi+1,ei);
+}
+int main(){
+    int arr[]={5,1,8,2,7,6,3,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    quickSort(arr,0,n-1);
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
