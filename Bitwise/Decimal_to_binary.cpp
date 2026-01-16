99657dd32d700b8b69df8f81beea7230b813ce02
sandhu2702 <sk0581822@gmail.com>
2026-01-16 19:58:58 +0530

    decimal_to_binary conversion added

---
+++ Bitwise/Decimal_to_binary.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+string decimal_to_binary(int num){
+    string result="";
+    while(num>0){
+        if(num%2==0){
+            //even
+            result="0"+result;
+        }
+        else{
+            //odd
+            result="1"+result;
+        }
+        num=num/2;
+    }
+    return result;
+}
+int main(){
+    int num;
+    cin>>num;
+    cout<<decimal_to_binary(num)<<"\n";
+    return 0;
+}
