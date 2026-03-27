99577a3382d83c829bad9452b7635012c4c65874
sandhu2702 <sk0581822@gmail.com>
2026-03-27 08:48:13 +0530

    Top View of Tree

--- maps/mapsbasics.cpp
+++ maps/mapsbasics.cpp
@@ -1,28 +1,14 @@
 #include<iostream>
-#include<unordered_map>
+#include<set>
+#include<map>
 using namespace std;
 int main(){
-    unordered_map<string,int> m;
-    // pair<string, int> p1;
-    // p1.first="raghav";
-    // p1.second=76;
-    // m.insert(p1);
-    // pair<string, int> p2;
-    // p2.first="harsh";
-    // p2.second=15;
-    // m.insert(p2);
-    // pair<string, int> p3;
-    // p3.first="lokesh";
-    // p3.second=49;
-    // m.insert(p3);
-    m["Harsh"]=15;
-    m["Raghav"]=20;
-    m["Suryansh"]=56;
-    for(auto p: m){
-        cout<<p.first<<" "<<p.second<<endl;
-    }
-    m.erase("Raghav");
-    for(auto p: m){
-        cout<<p.first<<" "<<p.second<<endl;
+    map<string,int>m;
+    m["Raghav"]=76;
+    m["Sandhya"]=10;
+    m["Rahul"]=52
+    ;
+    for(auto ele:m){
+        cout<<ele.second<<" ";
     }
 }
