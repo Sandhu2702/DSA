aa3c16eb413d734e8243574f84b28b479c2d130b
Sandhu2702 <sk0581822@gmail.com>
2026-09-04 18:06:37 +0530

    rotate_array_by_k modified

--- arrays/rotate_array_by_k.cpp
+++ arrays/rotate_array_by_k.cpp
@@ -25,25 +25,32 @@ int main(){
     //     cout<<a[i]<<" ";
     // }
 
-    //second approach--------
+    //second approach--------O(N+d)-time complexity and O(d)-space complexity
     //for k places 
     int k=3;
-    int temp[k];
-    for(int i=0;i<k;i++){
-        temp[i]=a[i];
-    }
-    for(int i=k;i<n;i++){
-        a[i-k]=a[i];
-    }
-    int j=0;
-    for(int i=n-k;i<n;i++){
-        a[i]=temp[j];
-        j++;
-    }
+    // int temp[k];
+    // for(int i=0;i<k;i++){
+    //     temp[i]=a[i];
+    // }
+    // for(int i=k;i<n;i++){
+    //     a[i-k]=a[i];
+    // }
+    // int j=0;
+    // for(int i=n-k;i<n;i++){
+    //     a[i]=temp[j];
+    //     j++;
+    // }
+    // for(int i=0;i<n;i++){
+    //     cout<<a[i]<<" ";
+    // }
+    
+    //optimal approach-------revresing part of array---O(2N)-time complexity but space complexity-O(1)
+    reverse(a.begin(),a.begin()+3);
+    reverse(a.begin()+3, a.end());
+    reverse(a.begin(),a.end());
     for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
     }
-
 
 
 
