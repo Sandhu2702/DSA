22b8cb7acf91ab36060fa415ea26624c4911d015
Sandhu2702 <sk0581822@gmail.com>
2026-09-05 18:02:25 +0530

    modified intersection of two sorted arrays

--- arrays/intersection_sorted_arrays.cpp
+++ arrays/intersection_sorted_arrays.cpp
@@ -9,18 +9,40 @@ int main(){
     int n1=sizeof(arr1)/sizeof(arr1[0]);
     int n2=sizeof(arr2)/sizeof(arr2[0]);
 
+    // int i=0;
+    // int j=0;
+    // vector<int>ans;
+    // int vis[n2]={0};
+    // for(int i=0;i<n1;i++){
+    //     for(int j=0;j<n2;j++){
+    //         if(arr1[i]==arr2[j] && vis[j]==0){
+    //             ans.push_back(arr1[i]);
+    //             vis[j]=1;
+    //             break;
+    //         }
+    //         if(arr2[j]>arr1[i]) break;
+    //     }
+    // }
+
+    // for(int i=0;i<ans.size();i++){
+    //     cout<<ans[i]<<" ";
+    // }
+
+    // second approach ----two pointer 
     int i=0;
     int j=0;
     vector<int>ans;
-    int vis[n2]={0};
-    for(int i=0;i<n1;i++){
-        for(int j=0;j<n2;j++){
-            if(arr1[i]==arr2[j] && vis[j]==0){
-                ans.push_back(arr1[i]);
-                vis[j]=1;
-                break;
-            }
-            if(arr2[j]>arr1[i]) break;
+    while(i<n1 && j<n2){
+        if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else if(arr2[j]<arr1[i]){
+            j++;
+        }
+        else{
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
         }
     }
 
