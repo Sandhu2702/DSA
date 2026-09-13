efa4bf7a31352eb0ca9500f883f78eecfc269d26
Sandhu2702 <sk0581822@gmail.com>
2026-09-13 12:57:38 +0530

    contained most water with two pointers approach

---
+++ arrays/Two_pointers/contained_most_water.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int height[]={1,8,6,2,5,4,8,3,7};
+    int n=sizeof(height)/sizeof(height[0]);
+    int maxArea=0;
+    int area=0;
+    int l=0;
+    int r=n-1;
+    while(l<r){
+        int h=min(height[l],height[r]);
+        int w=r-l;
+        area=h*w;
+        maxArea=max(maxArea,area);
+        if(height[l]<=height[r]){
+            l++;
+        }else{
+            r--;
+        }
+    }
+    cout<<"Most water contained will be:"<<maxArea;
+}
