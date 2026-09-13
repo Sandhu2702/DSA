f9158923453bee0f1bdeca6cb3fe0bdcdf7777f7
Sandhu2702 <sk0581822@gmail.com>
2026-09-13 12:59:25 +0530

    modified contained most water problem

--- arrays/Two_pointers/contained_most_water.cpp
+++ arrays/Two_pointers/contained_most_water.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main(){
     int height[]={1,8,6,2,5,4,8,3,7};
     int n=sizeof(height)/sizeof(height[0]);
+
+    //Optimized approach with O(N) time complexity
     int maxArea=0;
     int area=0;
     int l=0;
